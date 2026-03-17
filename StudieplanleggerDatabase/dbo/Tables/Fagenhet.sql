ae3b3ce917907d9eda4f71114a9f6d6225e5ad5b
Ola Viken <olaviken@hotmail.com>
2026-03-17 23:25:53 +0100

    Modified tables. added autoincrement for some and changed syntax when it came to primary key

--- StudieplanleggerDatabase/dbo/Tables/Fagenhet.sql
+++ StudieplanleggerDatabase/dbo/Tables/Fagenhet.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Fagenhet] (
-    [FagenhetId]   INT            NOT NULL,
+    [FagenhetId]   INT NOT NULL Identity(1,1) Primary Key,
     [Fagenhetnavn] NVARCHAR (255) NOT NULL,
     [Leder]        NVARCHAR (255) NOT NULL,
     [Beskrivelse]  NVARCHAR (255) NOT NULL,
     [InstituttID]  INT            NOT NULL,
-    PRIMARY KEY CLUSTERED ([FagenhetId] ASC),
+    
     FOREIGN KEY ([InstituttID]) REFERENCES [dbo].[Institutt] ([Id])
 );
 
