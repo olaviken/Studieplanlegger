7c856e6f6e32adb0d9f110fa3be0ef8f5676d4f4
Ola Viken <olaviken@hotmail.com>
2026-03-21 19:20:41 +0100

    worked on the syntax of the tables

--- StudieplanleggerDatabase/dbo/Tables/Ansatt.sql
+++ StudieplanleggerDatabase/dbo/Tables/Ansatt.sql
@@ -1,12 +1,12 @@
-﻿CREATE TABLE [dbo].[Ansatt] (
-    [Epost]            NVARCHAR (50)  NOT NULL Primary Key,
-    [Etternavn]        NVARCHAR (50)  NOT NULL,
-    [Fornavn]          NVARCHAR (50)  NOT NULL,
-    [Fodselsdato]      DATE           NOT NULL,
-    [Stillingsprosent] INT            NOT NULL,
-    [RNDProsent]       INT            NOT NULL,
-    [Spesialisering]   NVARCHAR (255) NOT NULL,
-    [Fagenhet]         INT            NOT NULL,
-    FOREIGN KEY ([Fagenhet]) REFERENCES [dbo].[Fagenhet] ([FagenhetId])
+﻿CREATE TABLE [dbo].Ansatt (
+    Epost            NVARCHAR (50)  NOT NULL Primary Key,
+    Etternavn          NVARCHAR (50)  NOT NULL,
+    Fornavn          NVARCHAR (50)  NOT NULL,
+    Fodselsdato      DATE           NOT NULL,
+    Stillingprosent INT            NOT NULL,
+    RNDProsent       INT            NOT NULL,
+    Spesialisering   NVARCHAR (255) NOT NULL,
+    FagenhetId         INT            NOT NULL,
+    FOREIGN KEY (FagenhetId) REFERENCES [dbo].[Fagenhet] ([FagenhetId])
 );
 
--- StudieplanleggerDatabase/dbo/Tables/Fakultet.sql
+++ StudieplanleggerDatabase/dbo/Tables/Fakultet.sql
@@ -1,8 +1,8 @@
-﻿CREATE TABLE [dbo].[Fakultet] (
-    [Organisasjonsnummer] INT            NOT NULL Primary Key,
-    [Navn]                NVARCHAR (255) NOT NULL,
-    [Leder]               NVARCHAR (255) NOT NULL,
-    [Beskrivelse]         NVARCHAR (255) NULL,
+﻿CREATE TABLE [dbo].Fakultet (
+    Organisasjonsnummer INT            NOT NULL Primary Key,
+    Navn                NVARCHAR (255) NOT NULL,
+    Leder               NVARCHAR (255) NOT NULL,
+    Beskrivelse         NVARCHAR (255) NULL,
     
 );
 
