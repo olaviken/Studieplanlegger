2e2f9082574d81904eeceb1b3a3ede7f0aec1de6
Ola Viken <olaviken@hotmail.com>
2026-03-21 19:38:22 +0100

    Created new table for aktivitet

---
+++ StudieplanleggerDatabase/dbo/Tables/Aktivitet.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].Aktivitet
+(
+	Id INT NOT NULL Identity(1,1) PRIMARY KEY,
+	Beskrivelse NVARCHAR(100) NOT NULL,
+	Dato DATE NOT NULL,
+	AntallTimer INT NOT NULL,
+	Undervisningskategori INT NOT NULL, 
+	Emne NVARCHAR(50) NOT NULL,
+	
+	Foreign Key (Emne) REFERENCES [dbo].Emne (Emnekode),
+	Foreign Key (Undervisningskategori) REFERENCES [dbo].Undervisningskategori (Id),
+
+)
