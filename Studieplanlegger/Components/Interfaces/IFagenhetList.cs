51019b9391fec4d2919b2f53d5c49272f3d34a3e
Ola Viken <olaviken@hotmail.com>
2026-03-30 21:58:32 +0200

    Created an interface for each list. placed it on serverside

---
+++ Studieplanlegger/Components/Interfaces/IFagenhetList.cs
@@ -0,0 +1,12 @@
+﻿using Studieplanlegger.Components.Classes;
+
+namespace Studieplanlegger.Components.Interfaces
+{
+    public interface IFagenhetList
+    {
+        List<Fagenhet> GetFagenheter();
+        void AddFagenhet(Fagenhet fagenhet);
+        void RemoveFagenhet(Fagenhet fagenhet);
+        void UpdateFagenhet(Fagenhet oldFagenhet, Fagenhet newFagenhet);
+    }
+}
---
+++ Studieplanlegger/Components/Interfaces/IFakultetList.cs
@@ -0,0 +1,12 @@
+﻿using Studieplanlegger.Components.Classes;
+
+namespace Studieplanlegger.Components.Interfaces
+{
+    public interface IFakultetList
+    {
+        List<Fakultet> GetFakulteter();
+        void AddFakultet(Fakultet fakultet);
+        void RemoveFakultet(Fakultet fakultet);
+        void UpdateFakultet(Fakultet oldFakultet, Fakultet newFakultet);
+    }
+}
