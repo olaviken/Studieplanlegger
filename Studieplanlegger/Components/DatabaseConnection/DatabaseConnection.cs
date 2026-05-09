28799a51e506d6882f01eef12af1a3cbb12d095d
Ola Viken <olaviken@hotmail.com>
2026-05-09 16:46:10 +0200

    Making connection to Postgress database

---
+++ Studieplanlegger/Components/DatabaseConnection/DatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+
+namespace Studieplanlegger.Components.DatabaseConnection
+{
+    
+
+    public class DatabaseConnection
+    {
+        
+
+        public void PostgresKontakt(String DatabaseLink)
+        {
+            NpgsqlConnection kontakt = new NpgsqlConnection(DatabaseLink);
+            kontakt.Open();
+        }
+    }
+}
